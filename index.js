dabda97e32dcf96b221cce21db635b1df8ef50ce
Həziyev Emin <113848558+EminHaziyev@users.noreply.github.com>
2025-03-18 23:13:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
-
+app.set("trust proxy", 1);
 
 
 
