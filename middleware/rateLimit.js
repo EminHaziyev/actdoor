4505cb2907ce5c705d1952dbc55026f05c555755
Həziyev Emin <113848558+EminHaziyev@users.noreply.github.com>
2025-03-18 23:43:45 +0400

    Update rateLimit.js

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 5*60 * 1000, // 5 minutes
-  max: 3, // Limit each IP to 3 requests per windowMs
+  max: 60, // Limit each IP to 3 requests per windowMs
   message: { error: 'Too many requests, please try again later.' },
   standardHeaders: true, // Sends rate limit info in headers
   legacyHeaders: false, // Disable `X-RateLimit-*` headers
