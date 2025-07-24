c6ce22f5e8c6335d0f5a805ea8d0e0169d26f57b
Həziyev Emin <mn05x.mail@gmail.com>
2025-07-24 16:01:32 +0400

    solved setActivity problem

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,55 +1,54 @@
-const express = require('express');
-const rateLimitMiddleware = require('../middleware/rateLimit');
-const User = require('../models/user');
-const authenticateWithToken = require('../middleware/authWithToken');
-const apiLimiter = require('../middleware/rateLimit');
+const express = require("express");
+const rateLimitMiddleware = require("../middleware/rateLimit");
+const User = require("../models/user");
+const authenticateWithToken = require("../middleware/authWithToken");
+const apiLimiter = require("../middleware/rateLimit");
 const router = express.Router();
 
+router.post("/setActivity", apiLimiter, authenticateWithToken, (req, res) => {
+  const apiKey = req.apiKey;
 
+  const activity = req.body.activity;
 
-router.post('/setActivity', apiLimiter, authenticateWithToken, (req, res) => {
-    const apiKey = req.apiKey;
+  User.findOne({ uniqueKey: apiKey })
+    .then((user) => {
+      if (!user) return res.status(404).json({ message: "User not found" });
 
-    const activity = req.body.activity;
-
-
-    User.updateOne({ uniqueKey: apiKey }, { activity })
-        .then(() => res.json({ message: 'Activity set successfully' }))
-        .catch(err => res.status(400).json({ message: 'Error setting activity' }));
-
+      user.activity = req.body.activity;
+      user.markModified("activity");
+      return user.save();
+    })
+    .then(() => res.json({ message: "Activity set successfully" }))
+    .catch((err) =>
+      res.status(400).json({ message: "Error setting activity" })
+    );
 });
 
-router.get('/getActivity/:username', async (req, res) => {
-    const username = req.params.username;
-    const type = req.query.type;
+router.get("/getActivity/:username", async (req, res) => {
+  const username = req.params.username;
+  const type = req.query.type;
 
-    const user = await User.findOne({ username });
-    if (!user) {
-        return res.status(400).json({ message: 'User not found' });
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  if (user.updatedAt && user.activity.time !== -1) {
+    const startTime = new Date(user.updatedAt);
+    const now = new Date();
+    const diffMs = now.getTime() - startTime.getTime();
+    if (diffMs > 60 * 1000) {
+      user.activity.time = -1;
+      await user.save();
     }
-    if (user.updatedAt && user.activity.time !== -1) {
-        
-        const startTime = new Date(user.updatedAt);
-        const now = new Date();
-        const diffMs = now.getTime() - startTime.getTime();
-        if (diffMs > 60 * 1000) { 
-            user.activity.time = -1;
-            await user.save();
-        }
-    }
+  }
 
-    if (type == "square") {
-        res.render('square', { activity: user.activity });
-    }
-    else if (type == "rectangle") {
-        res.render('rectangle', { activity: user.activity });
-    }
-    else{
-        res.json({ activity: user.activity });
-    }
-
+  if (type == "square") {
+    res.render("square", { activity: user.activity });
+  } else if (type == "rectangle") {
+    res.render("rectangle", { activity: user.activity });
+  } else {
+    res.json({ activity: user.activity });
+  }
 });
-
-
 
 module.exports = router;
