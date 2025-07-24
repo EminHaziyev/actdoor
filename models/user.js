5e4280dc083584073c0d51e213824474a721eb39
Həziyev Emin <mn05x.mail@gmail.com>
2025-07-24 15:15:02 +0400

    Added updatedAt field to schema for new logic

--- models/user.js
+++ models/user.js
@@ -16,12 +16,28 @@ const userSchema = new mongoose.Schema({
       fileName: "",
       time: 0,
       startTime: new Date(),
+      updatedAt: new Date()
     })
   },
   message: {
     type: Boolean
   }
-  
+});
+
+// Update updatedAt on activity change
+userSchema.pre('save', function(next) {
+  if (this.isModified('activity')) {
+    this.updatedAt = new Date();
+    if (this.activity && typeof this.activity === 'object') {
+      this.activity.updatedAt = new Date();
+    }
+  }
+  next();
+});
+
+// Add updatedAt field to schema
+userSchema.add({
+  updatedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
